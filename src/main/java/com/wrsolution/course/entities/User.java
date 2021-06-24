74e9a353fb10c409be9b21cfd07bb69ee896677b
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-06-24 12:45:18 -0300

    H2 database, test profile, JPA

--- src/main/java/com/wrsolution/course/entities/User.java
+++ src/main/java/com/wrsolution/course/entities/User.java
@@ -1,11 +1,18 @@
 package com.wrsolution.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
-public class User implements Serializable{	
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -66,7 +73,10 @@ public class User implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -78,9 +88,11 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-
 }
