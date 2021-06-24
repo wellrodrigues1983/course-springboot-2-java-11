f2819233870a43680cac827a3a47055d0f78c172
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-06-24 14:44:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/wrsolution/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.wrsolution.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wrsolution.course.entities.User;
+import com.wrsolution.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
