e83ada7e8c5f64b08f3248d338a4914019af3c00
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-07-01 13:14:46 -0300

    Payment, one-to-one association

--- src/main/java/com/wrsolution/course/config/TestConfig.java
+++ src/main/java/com/wrsolution/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wrsolution.course.entities.Category;
 import com.wrsolution.course.entities.Order;
 import com.wrsolution.course.entities.OrderItem;
+import com.wrsolution.course.entities.Payment;
 import com.wrsolution.course.entities.Product;
 import com.wrsolution.course.entities.User;
 import com.wrsolution.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
-		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
