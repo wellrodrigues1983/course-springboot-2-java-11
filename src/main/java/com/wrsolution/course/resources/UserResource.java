80f17784fc5ac70e842f7e6dad1694e53054c4d5
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-07-01 15:30:25 -0300

    User insert

--- src/main/java/com/wrsolution/course/resources/UserResource.java
+++ src/main/java/com/wrsolution/course/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.wrsolution.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wrsolution.course.entities.User;
 import com.wrsolution.course.services.UserService;
@@ -33,6 +37,15 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
 	}
 
 }
